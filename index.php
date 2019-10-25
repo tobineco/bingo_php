1aa822c4bf87c5c5fc3fac8b5345bef76cd47519
tobineco <feimao.318@gmail.com>
2019-10-25 10:45:11 +0000

    dot bingo-1

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="utf-8">
+        <title>BINGO!</title>
+    </head>
+    <body>
+        <div id="container">
+            <table>
+                <tr>
+                    <th>B</th><th>I</th><th>N</th><th>G</th><th>O</th>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>1</td>
+                    <td>1</td>
+                    <td>1</td>
+                    <td>1</td>
+                </tr>
+            </table>
+        </div>
+    </body>
+</html>
