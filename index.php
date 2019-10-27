7c60003440cfd3bdbbccd247b9cdf3663289bed3
tobineco <feimao.318@gmail.com>
2019-10-27 15:26:21 +0000

    bingo-3

--- index.php
+++ index.php
@@ -1,9 +1,32 @@
+<?php
+
+require_once(__DIR__ . '/config.php');
+require_once(__DIR__ . '/Bingo.php');
+
+$bingo = new \MyApp\Bingo();
+$nums = $bingo->create();
+
+/*
+$nums[][]
+B: $nums[0]  1-15
+I: $nums[1] 16-30
+..
+O: $nums[4]
+
+$nums[$i] $i*15+1 ～ $i*15+15
+*/
+
+// var_dump($nums);
+// exit;
+
+?>
+
 <!DOCTYPE html>
 <html lang="ja">
     <head>
         <meta charset="utf-8">
         <title>BINGO!</title>
-        <link rel="stylesheet" href="styles.css">
+        <link rel="stylesheet" href="./styles.css">
     </head>
     <body>
         <div id="container">
@@ -11,13 +34,13 @@
                 <tr>
                     <th>B</th><th>I</th><th>N</th><th>G</th><th>O</th>
                 </tr>
+                <?php for ($i = 0; $i < 5; $i++) : ?>
                 <tr>
-                    <td>1</td>
-                    <td>1</td>
-                    <td>1</td>
-                    <td>1</td>
-                    <td>1</td>
+                    <?php for ($j = 0; $j < 5; $j++) : ?>
+                    <td><?= h($nums[$i][$j]); ?></td>
+                    <?php endfor; ?>
                 </tr>
+            <?php endfor; ?>
             </table>
         </div>
     </body>
